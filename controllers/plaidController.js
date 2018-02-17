30855e82bac66a1316a1f1076fd9370f5a2a08f7
James Martineau <James.Martineau@libertymutual.com>
2018-02-16 19:40:33 -0500

    linking sandbox account to plaid is working.

--- controllers/plaidController.js
+++ controllers/plaidController.js
@@ -1,25 +1,44 @@
 require('dotenv').config();
 const axios = require('axios');
 const db = require("../models");
+var plaid = require('plaid');
 
 /* This should probably be globally availbale to our controller */
 var client = new plaid.Client(
   process.env.PLAID_CLIENT_ID, // these values need to be updated and stored in a .env
-  process.env.PLAID_SECRET,   // these values need to be updated and stored in a .env
+  process.env.PLAID_SECRET, // these values need to be updated and stored in a .env
   process.env.PLAID_PUBLIC_KEY, // these values need to be updated and stored in a .env
   plaid.environments.sandbox
 );
 
 // Defining methods for the plaidController
 module.exports = {
-  
-  // this is where our app will get it's private key that we persist to our db, probably need an "app model?"
-  getPrivateKey: function(req, res) {
-    
+
+  // this is where our app will get it's private access key that we persist to our db, probably need an "app model?"
+  getPrivateKey: function (req, res) {
+
+    PUBLIC_TOKEN = req.body.public_token;
+    client.exchangePublicToken(PUBLIC_TOKEN, function (error, tokenResponse) {
+      if (error != null) {
+        var msg = 'Could not exchange public_token!';
+        console.log(msg + '\n' + error);
+        return res.json({
+          error: msg
+        });
+      }
+      ACCESS_TOKEN = tokenResponse.access_token;
+      ITEM_ID = tokenResponse.item_id;
+
+      console.log('Access Token: ' + ACCESS_TOKEN);
+      console.log('Item ID: ' + ITEM_ID);
+      res.json({
+        'error': false
+      });
+    });
   },
 
   // this is where we get items (accounts)
-  getItem: function(req, res) {
+  getItem: function (req, res) {
     /* This is boilerplate from the API */
     // Pull information about the Item
     client.getItem(ACCESS_TOKEN, (err, result) => {
@@ -31,7 +50,7 @@ module.exports = {
   },
 
   // this is where we get transactions
-  getTransactions: function(req, res){
+  getTransactions: function (req, res) {
     /* This is boilerplate from the API */
 
     // Retrieve transactions from Jan 1 until Feb 15
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
+// const bookRoutes = require("./books");
+const plaidRoutes = require("./plaid");
 
 // Book routes
-router.use("/books", bookRoutes);
+// router.use("/books", bookRoutes);
+router.use("/plaid", plaidRoutes);
 
 module.exports = router;
