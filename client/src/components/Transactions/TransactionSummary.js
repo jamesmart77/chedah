4c8ffd65f81218f979de3d1486d0b9b0118393eb
michellechristina <michelle@thisisbam.com>
2018-03-01 12:24:16 -0500

    added gig detail logic, refactored gig detail components, added react data grid for transactions component, added placeholders for gig detail functions

--- client/src/components/Transactions/TransactionSummary.js
+++ client/src/components/Transactions/TransactionSummary.js
@@ -2,82 +2,11 @@ import React, { Component } from "react";
 import Transaction from './Transaction';
 
 
-class TransactionSummary extends Component {
-
-    state = {
-        gig: { 
-            gigName: "Uber",  
-          },
-         transactions: [
-            {
-              id: 1,
-              date: "11/23/14", 
-              vendor: "Google", 
-              category: "Advertising", 
-              gig: "Uber",
-              amount: 200.00
-            },
-            {
-              id: 2,
-              date: "11/23/14", 
-              vendor: "Staples", 
-              category: "Office Supplies", 
-              gig: "Uber",
-              amount: 15.00
-            },
-            {
-              id: 3,
-              date: "11/23/14", 
-              vendor: "Staples", 
-              category: "Office Supplies", 
-              gig: "Dev",
-              amount: 12.00
-            },
-            {
-              id: 4,
-              date: "11/23/14", 
-              vendor: "Staples", 
-              category: "Office Supplies", 
-              gig: "Uber",
-              amount: 16.00
-            },
-            {
-              id: 5,
-              date: "11/23/14", 
-              vendor: "Staples", 
-              category: "Office Supplies",
-              gig: "Dev", 
-              amount: 119.00
-            },
-            {
-              id: 6,
-              date: "11/23/14", 
-              vendor: "Staples", 
-              category: "Office Supplies", 
-              gig: "Dev",
-              amount: 219.00
-            },
-            {
-              id: 7,
-              date: "11/23/14", 
-              vendor: "Staples", 
-              category: "Office Supplies", 
-              gig: "Uber",
-              amount: 29.00
-            }
-          ]
-    };
-
-
-    
-
-    render() {
-        return (
-            <div className="card">
+const TransactionSummary = props => <div className="card">
             <div className="card-content cardHeader">
                 <div className="row">
                     <div className="col s12">
-                        <span className="card-title"><span className="primaryHeaderText">Transactions: </span> <span className="secondaryHeaderText">{this.state.gig.gigName}</span></span>
+                        <span className="card-title"><span className="primaryHeaderText">Transactions: </span> <span className="secondaryHeaderText">{'Uber'}</span></span>
                     </div>
                     
                 </div>
@@ -86,37 +15,9 @@ class TransactionSummary extends Component {
              <div className="card-content cardBody">
                 <div className="row">
                     <div className="col s12">
-                
-                      <table className="striped">
-                        <thead>
-                          <tr>
-                            <td>Date</td>
-                            <td>Vendor</td>
-                            <td>Category</td>
-                            <td>Gig</td>
-                            <td>Amount</td>
-                          </tr>
-                        </thead>
-                        <tbody>
-                        
-                       
-                {this.state.transactions.map((transaction) => (
-                    <Transaction
-                       
-                        date={transaction.date}
-                        vendor={transaction.vendor}
-                        category={transaction.category}
-                        gig={transaction.gig}
-                        amount={transaction.amount}
-                       
-                    />
-                ))}
-           
-                        
-                     
-                        </tbody>
-                      </table>
-    
+                    {console.log('props.data')}
+                    {console.log(props.data)}
+                    <Transaction {...props} />
                 
                     </div>
     
@@ -124,10 +25,5 @@ class TransactionSummary extends Component {
                   </div>
                   </div>
            
-
-        );
-    }
-}
-
 
 export default TransactionSummary;
