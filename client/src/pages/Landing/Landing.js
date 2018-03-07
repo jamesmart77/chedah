215cb03e52c3de98a9294cea5f91df1ba4350dc3
michellechristina <michelle@thisisbam.com>
2018-03-06 20:49:19 -0500

    landing page laid out

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -11,10 +11,61 @@ class Landing extends Component {
 
   render() {
     return (
-      <Container>
-        <h1>Landing Page Goes Here</h1>
+      <div>
+        <div className="row header">
+        <div className="col s12">
+          <h1 className="headerTitle">Chedah</h1>
+          <h4 className="headerTitle">TagLine Goes Here. TagLine Goes Here</h4>
+          <h6 className="headerTitle">Sign Up / Sign In</h6>
+        </div>
+        </div>
+       
 
-      </Container>
+          <div className="row header2">
+
+            <div className="col s12 m4">
+            <img src='https://placehold.it/250x250//cccccc' alt=''/>
+              <h6>Promo Content goes here</h6>
+            </div>
+            <div className="col s12 m4">
+            <img src='https://placehold.it/250x250//cccccc' alt=''/>
+            <h6>Promo Content goes here</h6>
+            </div>
+            <div className="col s12 m4">
+            <img src='https://placehold.it/250x250//cccccc' alt=''/>
+            <h6>Promo Content goes here</h6>
+            </div>
+        </div>
+
+        <div className="row header valign-wrapper">
+          <div className="col s12">
+              <h6 className="headerTitle">Parallaxy Thing</h6>
+          </div>
+        </div>
+
+        <div className="row header2">
+
+          <div className="col s12 m6">
+          <img src='https://placehold.it/450x250//cccccc' alt=''/>
+          </div>
+          <div className="col s12 m6">
+          <h4>Promo Content goes here</h4>
+          <ul>
+            <li>A Thing</li>
+            <li>A Thing</li>
+            <li>A Thing</li>
+            </ul>
+          </div>
+          <div className="col s12">
+          <h4>Pull Quote About Money </h4>
+          </div>
+        </div>
+
+
+      </div>
+
+       
+
     );
   }
 }
