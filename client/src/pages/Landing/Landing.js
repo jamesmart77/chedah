248a7f911ab66df432bc88a24f3603e71a99d2cd
michellechristina <michelle@thisisbam.com>
2018-03-08 10:52:53 -0500

    styles added

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -23,17 +23,17 @@ freelance gig, your side hustle, all your entreprenuerial endeavors</h6>
             <div className="col s12 m4 landingImg1">
             <img src='../../assets/img/product1.png' alt=''/>
               <h6 className="productTitle">One dashboard</h6>
-              <h6 className="productText">Easily sync all of your financial accounts in one place. Keep a top level view of your money</h6>
+              <h6 className="productText">Easily sync all your financial accounts in one place. Keep a top level view of your money.</h6>
             </div>
             <div className="col s12 m4 landingImg2">
             <img src='../../assets/img/product2.png' alt=''/>
             <h6 className="productTitle">Track Your Gigs </h6>
-            <h6 className="productText">Break out all of your expenses and income for each gig and project in your pipeline</h6>
+            <h6 className="productText">Break out all of your expenses and income for each gig and project in your pipeline.</h6>
             </div>
             <div className="col s12 m4 landingImg3">
             <img src='../../assets/img/product3.png' alt=''/>
             <h6 className="productTitle">Set Milestones </h6>
-            <h6 className="productText">Customize goals and budgets for each gig. Track your most profitable projects </h6>
+            <h6 className="productText">Customize goals and budgets for each gig. Track your most profitable projects. </h6>
             </div>
         </div>
 
@@ -52,11 +52,11 @@ freelance gig, your side hustle, all your entreprenuerial endeavors</h6>
           <div className="col s12 m7">
           <div className="landingListTitle">Your Finances. Personalized.</div>
           <ul className="landingList">
-            <li><h6><i className="material-icons tiny">check_circle</i> Simple expense and payment tracking for every project you're working on.</h6></li>
-            <li><h6><i className="material-icons tiny">check_circle</i> Capture all the information you need to reveal your most profitable projects.</h6></li>
-            <li><h6><i className="material-icons tiny">check_circle</i> Streamline your workflow with one dashboard to rule them all.</h6></li>
-            <li><h6><i className="material-icons tiny">check_circle</i> Create goals and easily monitor results. </h6></li>
-            <li><h6><i className="material-icons tiny">check_circle</i> Take control of your finances - no complicated accounting software needed. </h6></li>
+            <li><h6><i className="material-icons tiny teal-text">check_circle</i> Simple expense and payment tracking for every project you're working on.</h6></li>
+            <li><h6><i className="material-icons tiny teal-text">check_circle</i> Capture all the information you need to reveal your most profitable projects.</h6></li>
+            <li><h6><i className="material-icons tiny teal-text">check_circle</i> Streamline your workflow with one dashboard to rule them all.</h6></li>
+            <li><h6><i className="material-icons tiny teal-text">check_circle</i> Create goals and easily monitor results. </h6></li>
+            <li><h6><i className="material-icons tiny teal-text">check_circle</i> Take control of your finances - no complicated accounting software needed. </h6></li>
          
             </ul>
           </div>
