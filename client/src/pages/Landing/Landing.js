a7a9354265a58fc9b15fc8dfc3b08faadc27c9e5
michellechristina <michelle@thisisbam.com>
2018-03-07 23:54:57 -0500

    fancy things added

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -11,14 +11,14 @@ class Landing extends Component {
         <div className="col s12">
           <div className="titleHead">simple money management <br/> for the gig economy</div>
           <h6 className="headerTitle">track your
-freelance gig, your side hustle, your entreprenuerial endeavors</h6>
+freelance gig, your side hustle, all your entreprenuerial endeavors</h6>
           {/* <Button className="teal" waves='light'>Sign Up</Button> */}
           <h6 className="headerTitle">Sign Up / Sign In</h6>
         </div>
         </div>
        
 
-          <div className="row header2">
+          <div className="row header1">
 
             <div className="col s12 m4 landingImg1">
             <img src='../../assets/img/product1.png' alt=''/>
@@ -28,12 +28,12 @@ freelance gig, your side hustle, your entreprenuerial endeavors</h6>
             <div className="col s12 m4 landingImg2">
             <img src='../../assets/img/product2.png' alt=''/>
             <h6 className="productTitle">Track Your Gigs </h6>
-            <h6 className="productText">Break out all of your expenses and income for each gigs and project in your pipeline</h6>
+            <h6 className="productText">Break out all of your expenses and income for each gig and project in your pipeline</h6>
             </div>
             <div className="col s12 m4 landingImg3">
             <img src='../../assets/img/product3.png' alt=''/>
             <h6 className="productTitle">Set Milestones </h6>
-            <h6 className="productText">Customize goals and budgets for each gig. Track your most profitable projects. </h6>
+            <h6 className="productText">Customize goals and budgets for each gig. Track your most profitable projects </h6>
             </div>
         </div>
 
@@ -46,21 +46,27 @@ freelance gig, your side hustle, your entreprenuerial endeavors</h6>
 
         <div className="row header2">
 
-          <div className="col s12 m6">
-          <img className="landingImg" src='https://placehold.it/450x250//cccccc' alt=''/>
+          <div className="col s12 m5">
+          <img src='../../assets/img/product4.png' alt=''/>
           </div>
-          <div className="col s12 m6">
-          <h4>Promo Content goes here</h4>
+          <div className="col s12 m7">
+          <div className="landingListTitle">Your Finances. Personalized.</div>
           <ul className="landingList">
-            <li><h5><i className="material-icons">check_circle</i>A Thing</h5></li>
-            <li><h5><i className="material-icons">check_circle</i>Another Thing</h5></li>
-            <li><h5><i className="material-icons">check_circle</i>A New Nother Thing</h5></li>
+            <li><h6><i className="material-icons tiny">check_circle</i> Simple expense and payment tracking for every project you're working on.</h6></li>
+            <li><h6><i className="material-icons tiny">check_circle</i> Capture all the information you need to reveal your most profitable projects.</h6></li>
+            <li><h6><i className="material-icons tiny">check_circle</i> Streamline your workflow with one dashboard to rule them all.</h6></li>
+            <li><h6><i className="material-icons tiny">check_circle</i> Create goals and easily monitor results. </h6></li>
+            <li><h6><i className="material-icons tiny">check_circle</i> Take control of your finances - no complicated accounting software needed. </h6></li>
+         
             </ul>
           </div>
-          <div className="col s12">
-          <h4>Pull Quote About Money</h4>
           </div>
-        </div>
+
+
+          {/* <div className="col s12">
+          <h4>Pull Quote About Money</h4>
+          </div> */}
+       
 
 
       </div>
