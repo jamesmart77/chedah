5f3a323e09f93243fe4767011e316b438b071cfe
Michael Fessenden <michael.fessenden@gmail.com>
2018-03-10 00:51:13 -0500

    update gig menu

--- client/src/components/GigMenu.js
+++ client/src/components/GigMenu.js
@@ -1,33 +1,51 @@
 import React from 'react';
-import API from '../../utils/API';
-
-/*
-<ul id="desktop-main-menu" className="dropdown-content">
-    <li><a href="/accounts"><i className="large material-icons">account_balance</i>Accounts</a></li>
-    <li><a href="#!"><i className="large material-icons">work</i>Gigs</a></li>
-    <li><a href="#!"><i className="large material-icons">insert_chart</i>Goals</a></li>
-    <li className="divider"></li>
-    <li><a href="#!" onClick={() => API.accountsSync()}><i className="large material-icons">sync</i>Sync Accounts</a></li>
-    <li className="divider"></li>
-    <PlaidAccountLink><i className="large material-icons">insert_link</i>Link Account</PlaidAccountLink>
-    <li className="divider"></li>
-    <li><a href="#!" onClick={() => logout()}><i className="large material-icons">keyboard_tab</i>Log Out</a></li>
-    <li className="divider"></li>
-</ul>
-*/
+import API from '../utils/API';
 
 
+// initialize by passing a `gigs` argument consisting of {name:, _id:}
 class GigMenu extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            name: 'Gig:',
+            id: null
+        }
+    }
+
+    // menu change event
+    menuChanged(event) {
+        this.setState({name: event.target.getAttribute('name'), id: event.target.getAttribute('datavalue')})
+    }
+
+    // reset component
+    reset() {
+        this.setState({name: 'Gig:', id: null})
+    }
+
+    // render gig menu items
+    renderListItems() {
+        const gigs = this.props.gigs || []
+        let listItems = gigs.map(gig =>
+            <li key={gig._id}>
+                <a onClick={this.menuChanged.bind(this)} name={gig.name} datavalue={gig._id} href="#!" style={{color: '#ababab69'}}>{gig.name}</a>
+            </li>
+        )
+
+        listItems.unshift(<li key={0}><a  onClick={this.reset.bind(this)} href="#!">None</a></li>)
+        return listItems
     }
 
     render() {
+        const menuId = `menu-gig-` + this.props._id
+
         return (
-            <a data-activates={`menu-gig-` + this.props.id} className="button-collapse"><i className="material-icons">{this.props.name}</i></a>
-            <select>
-            </select>
+            <div>
+                <a className="dropdown-button" href="#!" data-activates={menuId} style={{maxWidth: '200'}}>{this.state.name}<i className="material-icons right">arrow_drop_down</i></a>
+                <ul id={menuId} className="dropdown-content">
+                    {this.renderListItems()}
+                </ul>
+            </div>
         );
     }
 }
