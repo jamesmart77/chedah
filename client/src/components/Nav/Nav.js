29f4b6a9e433af90aa2f99d5d3b5c4a08fe4b216
michellechristina <michelle@thisisbam.com>
2018-03-09 19:39:46 -0500

    mobile nav menu removed from landing page

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,42 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { login, isLoggedIn } from '../../utils/AuthService';
 import MainMenu from './MainMenu';
 
 
-
 // materialize navbar
-class Nav extends Component {
+const Nav = () => (
+    <header>
+        <nav className="navbar deep-orange darken-2">
 
-    state = {
-        user: {}
-    }
+            {/* <!-- Navigation Bar --> */}
+            <div className="nav-wrapper">
+                <a href="/dashboard" className="brand-logo header-logo"><img src="/assets/img/icon-24x24.svg" alt='chedah'/> chedah</a>
+                {/* <!-- Hamburger Menu Icon --> */}
+                {/* <a href="#!" data-activates="mobile-main-menu" className="button-collapse"><i className="material-icons">menu</i></a> */}
+                <MainMenu/>
+                {(isLoggedIn()) ?
+                <div>
+                     {/* <!-- Hamburger Menu Icon --> */}
+                    <a href="#!" data-activates="mobile-main-menu" className="button-collapse"><i className="material-icons">menu</i></a>
+               
+                <ul className="right topnav-menu">
+                    <li><a className="dropdown-button hide-on-med-and-down" href="#!" data-activates="desktop-main-menu">Menu<i className="material-icons right">arrow_drop_down</i></a></li>
+                    <li><a href="#" data-activates="user-slideout" className="waves-effect waves-block waves-light user-side-collapse"><i className="material-icons">input</i></a></li>
+                </ul>
+                </div>
+                :
+                <ul className="right topnav-menu">
+                    <li><a href="#!" onClick={() => login()}>Log In</a></li>
+                </ul>}
 
-    render() {
-        return (
-            <header>
-                <nav className="navbar deep-orange darken-2">
+            </div>
+        </nav>
+    </header>
+    )
 
-                    {/* <!-- Navigation Bar --> */}
-                    <div className="nav-wrapper">
-                        <a href="/dashboard" className="brand-logo header-logo"><img src="/assets/img/icon-24x24.svg" alt='chedah'/> chedah</a>
-                        {/* <!-- Hamburger Menu Icon --> */}
-                        <a href="#!" data-activates="mobile-main-menu" className="button-collapse"><i className="material-icons">menu</i></a>
-                        <MainMenu/>
-                        {(isLoggedIn()) ?
-                        <ul className="right topnav-menu">
-                            <li><a className="dropdown-button hide-on-med-and-down" href="#!" data-activates="desktop-main-menu">Menu<i className="material-icons right">arrow_drop_down</i></a></li>
-                            <li><a href="#" data-activates="user-slideout" className="waves-effect waves-block waves-light user-side-collapse"><i className="material-icons">input</i></a></li>
-                        </ul>
-                        :
-                        <ul className="right topnav-menu">
-                            <li><a href="#!" onClick={() => login()}>Log In</a></li>
-                        </ul>}
-
-                    </div>
-                </nav>
-            </header>
-        )
-    }
-}
 
 export default Nav;
