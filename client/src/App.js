2329d88c00dc084767fd1d60a9b5c3111ccac892
michellechristina <michelle@thisisbam.com>
2018-03-09 19:34:00 -0500

    removed breadcrumbs from landing page

--- client/src/App.js
+++ client/src/App.js
@@ -42,7 +42,8 @@ class App extends React.Component {
   render() { return <Router history={history}>
     <div>
       <Nav user={this.state.user} />
-      <Breadcrumbs location={history.location}/>
+      {/* <Breadcrumbs location={history.location}/> */}
+      {history.location.pathname !== '/' && <Breadcrumbs location={history.location}/> }
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/callback" component={Callback} />
