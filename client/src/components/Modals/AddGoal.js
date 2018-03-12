ef0bccd650dcc914a0445055e62746abd187b681
michellechristina <michelle@thisisbam.com>
2018-03-11 21:43:02 -0400

    added plaid categories to add goal modal

--- client/src/components/Modals/AddGoal.js
+++ client/src/components/Modals/AddGoal.js
@@ -1,131 +1,111 @@
-import React from 'react';
-import API from '../../utils/API';
-
-
-
-const defaultCategories = [
-    {
-        id: '9DF04A0DD19B2',
-        name: 'Travel'
-    }, {
-        id: 'DCCB4D8F37DA3',
-        name: 'Tolls'
-    }, {
-        id: 'D2A4A8A93CA476',
-        name: 'Gas'
-    }, {
-        id: 'C86E4E76D90B4',
-        name: 'Gas'
-    },{
-        id: '9DF0A40DD19B2',
-        name: 'Travel'
-    }, {
-        id: 'DCCBD8F437DA3',
-        name: 'Tolls'
-    }, {
-        id: 'D24AA8A93CA76',
-        name: 'Gas'
-    }, {
-        id: 'C846EE76D90B4',
-        name: 'Gas'
-    }, {
-        id: '9DF40A0DD129B2',
-        name: 'Travel'
-    }, {
-        id: 'DCCBD48F37DA3',
-        name: 'Tolls'
-    }, {
-        id: 'D2AA84A93CA76',
-        name: 'Gas'
-    }, {
-        id: 'C86EE76D940B4',
-        name: 'Gas'
-    },
-    {
-        id: '9DF40A0DD19B2',
-        name: 'Travel'
-    }, {
-        id: 'DCCBD8F37DA3',
-        name: 'Tolls'
-    }, {
-        id: 'D2AA48A93CA476',
-        name: 'Gas'
-    }
-]
+import React from 'react'
+import API from '../../utils/API'
 
 // add goal modal
 class ModalAddGoal extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.handleClick = this.handleClick.bind(this);
-    }
+  constructor (props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
 
-    componentWillMount() {
-        API.getGigData(this.props.gigId).then(gigData => {
-            this.setState(gigData)
-            // console.log(`-> ModalAddGoal: `, gigData);
-        }).catch(console.log)
-    }
+  state = {
+    categories: []
+  }
 
-    addGoalToGig() {
-        const data = {}
+  componentWillMount () {
+    API.getUserCategories()
+      .then(({data}) => {
+        this.setState({categories: data})
+        console.log('LOOK HERE categories!')
+        console.log(data)
+      }).catch(err => {
+      console.log('Error Categories')
+      console.log(err)
+    })
 
-        data.gigId = this.props.gigId
-        data.goal = {}
+    API.getGigData(this.props.gigId).then(gigData => {
+      this.setState(gigData)
+    // console.log(`-> ModalAddGoal: `, gigData)
+    }).catch(console.log)
+  }
 
-        API.addGoalToGig(data).then(res => {
-            this.loadGig();
-        }).catch(err => {
-            alert('what happened?')
-        })
-    }
+  addGoalToGig () {
+    const data = {}
 
-    handleClick(val) {
-        console.log(`selected: `, val);
-    }
+    data.gigId = this.props.gigId
+    data.goal = {}
 
-    render() {
-        const listItems = defaultCategories.map((item, i) =>
-            <option key={item.id}>{item.name}</option>
-        )
+    API.addGoalToGig(data).then(res => {
+      this.loadGig()
+    }).catch(err => {
+      alert('what happened?')
+    })
+  }
 
-        return (
-            <div id="add-goal-modal" className="modal" data-modal="data-modal" style={{height: '70%'}}>
-            <div className="modal-content">
-                <div className="modal-title">
-                    <h4>Add a Goal</h4>
-                </div>
-                <div className="col input-field s12">
-                    <input type="text" name="goalName" id="input_1" defaultValue={this.props.goalName}/>
-                    <label className="active" htmlFor="input_1">Goal Name</label>
-                </div>
-                <div className="col input-field s12">
-                    <input type="text" name="goalBudget" id="input_2" defaultValue={this.props.budget}/>
-                    <label className="active" htmlFor="input_2">Budget</label>
-                </div>
-                <div className="row">
-                    <span>Select Expense Categories To Track:</span>
-                </div>
-                <div className="row">
-                    <div className="col s6">
-                        <select onClick={this.handleClick} multiple="multiple">
-                            {listItems}
-                        </select>
-                    </div>
-                    <div className="col s6"></div>
-                </div>
-                <div className="modal-footer">
-                    <section>
-                        <button className="btn waves-effect waves-light btn-flat modal-action modal-close deep-orange darken-3 white-text">Cancel</button>
-                        &nbsp;
-                        <button onClick={() => this.addGoalToGig(this.props.gigId)} className="btn waves-effect waves-light modal-action modal-close teal">Apply</button>
-                    </section>
-                </div>
+  handleClick (val) {
+    console.log(`selected: `, val)
+  }
+
+  render () {
+    return (
+      <div
+        id='add-goal-modal'
+        className='modal'
+        data-modal='data-modal'
+        style={{height: '70%'}}>
+        <div className='modal-content'>
+          <div className='modal-title'>
+            <h4>Add a Goal</h4>
+          </div>
+          <div className='col input-field s12'>
+            <input
+              type='text'
+              name='goalName'
+              id='input_1'
+              defaultValue={this.props.goalName} />
+            <label className='active' htmlFor='input_1'>
+              Goal Name
+            </label>
+          </div>
+          <div className='col input-field s12'>
+            <input
+              type='text'
+              name='goalBudget'
+              id='input_2'
+              defaultValue={this.props.budget} />
+            <label className='active' htmlFor='input_2'>
+              Budget
+            </label>
+          </div>
+          <div className='row'>
+            <span>Select Expense Categories To Track:</span>
+          </div>
+          <div className='row'>
+            <div className='col s6'>
+              <select onClick={this.handleClick} multiple='multiple'>
+                {this.state.categories.map(category => <option value={category._id} key={category._id}>
+                                                         {category.name}
+                                                       </option>)}
+              </select>
             </div>
+            <div className='col s6'></div>
+          </div>
+          <div className='modal-footer'>
+            <section>
+              <button className='btn waves-effect waves-light btn-flat modal-action modal-close deep-orange darken-3 white-text'>
+                Cancel
+              </button>
+              &nbsp;
+              <button onClick={() => this.addGoalToGig(this.props.gigId)} className='btn waves-effect waves-light modal-action modal-close teal'>
+                Apply
+              </button>
+            </section>
+          </div>
         </div>
-        );
-    }
+      </div>
+    )
+  }
 }
 
-export default ModalAddGoal;
+export default ModalAddGoal
