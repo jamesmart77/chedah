071e0cd9fce1d1eea7bf5fff2a096ff49f474ddf
Benjamin Baumann <benjaminbaumann@bens-imac.thisisbam.com>
2018-03-14 15:14:42 -0400

    fixed create category bug

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,9 @@ class App extends React.Component {
 
   getUser(){
     API.getUser()
-      .then(user => this.setState({user: user.data}))
+      .then(user => {
+        this.setState({user: user.data})
+      })
       .catch(err => {
         console.log('error getting the user in the app.js file')
         console.log(err)
@@ -80,7 +82,7 @@ class App extends React.Component {
       {/* We don't want these modals loaded into the DOM unless the user is logged in, the reason for this is because they attempt to query the backend and can crash the progra without a token */}
       {isLoggedIn() && <div>
       <ModalEditAccount user={this.state.user}/>
-      <ModalAddGoal user={ this.state.user } location={ history.location } refresh={ this.refresh.bind(this) } mode={ 'add' } id={'add-goal-modal'} />
+      <ModalAddGoal user={ this.state.user } location={ history.location } refresh={ this.refresh.bind(this) } />
       <ModalAddGig user={this.state.user} refresh={ this.refresh.bind(this) }/>
       <ModalAddCategory user={this.state.user}/>
       </div>
