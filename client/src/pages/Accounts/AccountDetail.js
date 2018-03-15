592be1b057fcbe592fbc9019b700cf53bb24514c
Benjamin Baumann <benjaminbaumann@bens-imac.thisisbam.com>
2018-03-15 11:52:07 -0400

    built

--- client/src/pages/Accounts/AccountDetail.js
+++ client/src/pages/Accounts/AccountDetail.js
@@ -67,7 +67,6 @@ class AccountDetail extends Component {
             gigs.data.forEach(gig => {
                 gigData.push({name: gig.name, id: gig._id, description: gig.description})
             })
-            alert(JSON.stringify(gigData, null, 2))
             this.setState({gigs: gigData})
         })
         .catch(err => {
